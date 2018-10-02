2c35f911d4f437603ff447d82f9f413b85829eea
= <abirenbaum721@gmail.com>
2018-10-02 17:42:14 -0500

    Adding run

---
+++ public/project/run-tracker/server.R
@@ -0,0 +1,68 @@
+
+
+path_to_data <- ifelse(Sys.info()['nodename'] =="adamubuntu", "~/adambirenbaum.com/public/project/run-tracker/", "/var/www/adambirenbaum.com/public/project/run-tracker/")
+
+server <- function(input,output){
+  
+  output$plot <- renderPlot({
+    miles_df <- read.csv(paste0(path_to_data,"total_miles.csv"),stringsAsFactors = F,colClasses = c("Date","numeric","numeric","numeric"))
+    
+    miles_df <- miles_df %>% filter(Date <= Sys.Date()) %>% mutate(Bo = cumsum(Bo),
+                                                                   Rachael= cumsum(Rachael),
+                                                                   Adam = cumsum(Adam)) %>% 
+      gather(key = "Person",value = "Miles",Bo,Rachael,Adam)
+  
+    ggplot(miles_df, aes(x = Date, y = Miles, group = Person, color = Person)) + geom_line(size = 2) + theme_minimal() + ggtitle("Running Progress")+
+      theme(axis.text = element_text(size = 14), axis.title = element_text(size = 18),plot.title = element_text(size = 28),legend.text = element_text(size = 18))
+  })
+  
+  output$table <- renderTable({
+    miles_df <- read.csv(paste0(path_to_data,"total_miles.csv"),stringsAsFactors = F,colClasses = c("Date","numeric","numeric","numeric"))
+    miles_df <- miles_df %>% filter(Date <= Sys.Date()) 
+    num_days <- nrow(miles_df) - 1
+    table_df <- data.frame(Runner = c("Bo","Rachael","Adam"), "Total Miles" = c(sum(miles_df$Bo),sum(miles_df$Rachael), sum(miles_df$Adam)))
+    table_df <- table_df %>% mutate("Miles/Day" = Total.Miles/num_days)
+    table_df[["Longest Run"]] <- c(max(miles_df$Bo),max(miles_df$Rachael), max(miles_df$Adam))
+    table_df <- table_df[order(table_df$Total.Miles,decreasing = T),]
+    names(table_df)[2] <- "Total Miles"
+    table_df
+    
+  })
+  
+  
+  observeEvent(input$enter,{
+    miles_df <- read.csv(paste0(path_to_data,"total_miles.csv"),stringsAsFactors = F,colClasses = c("Date","numeric","numeric","numeric"))
+    
+
+    miles_df[[input$person]][miles_df$Date == Sys.Date()] <- input$miles +   miles_df[[input$person]][miles_df$Date == Sys.Date()]
+    
+    
+    write.csv(miles_df,paste0(path_to_data,"total_miles.csv"),row.names = F)
+    output$plot <- renderPlot({
+
+      miles_df <- miles_df %>% filter(Date <= Sys.Date()) %>% mutate(Bo = cumsum(Bo),
+                                                                     Rachael= cumsum(Rachael),
+                                                                     Adam = cumsum(Adam)) %>% 
+        gather(key = "Person",value = "Miles",Bo,Rachael,Adam)
+      
+      ggplot(miles_df, aes(x = Date, y = Miles, group = Person, color = Person)) + geom_line(size = 2) + theme_minimal() + ggtitle("Running Progress")+
+        theme(axis.text = element_text(size = 14), axis.title = element_text(size = 18),plot.title = element_text(size = 28),legend.text = element_text(size = 18))
+    })
+    
+    output$table <- renderTable({
+      miles_df <- read.csv(paste0(path_to_data,"total_miles.csv"),stringsAsFactors = F,colClasses = c("Date","numeric","numeric","numeric"))
+      miles_df <- miles_df %>% filter(Date <= Sys.Date()) 
+      num_days <- nrow(miles_df) - 1
+      table_df <- data.frame(Runner = c("Bo","Rachael","Adam"), "Total Miles" = c(sum(miles_df$Bo),sum(miles_df$Rachael), sum(miles_df$Adam)))
+      table_df <- table_df %>% mutate("Miles/Day" = Total.Miles/num_days)
+      table_df[["Longest Run"]] <- c(max(miles_df$Bo),max(miles_df$Rachael), max(miles_df$Adam))
+      table_df <- table_df[order(table_df$Total.Miles,decreasing = T),]
+      names(table_df)[2] <- "Total Miles"
+      table_df
+      
+    })
+    
+    
+  })
+  
+}
---
+++ public/project/run-tracker/ui.R
@@ -0,0 +1,55 @@
+library(shiny)
+library(dplyr)
+library(tidyr)
+library(ggplot2)
+
+ui <- fluidPage(
+  fluidRow(
+    column(
+      width = 4,
+      sliderInput("miles",h3("Miles"), min = 0, max = 10, step = 0.1,value = 3)
+      
+    ),
+    column(width = 4,
+           shinyWidgets::radioGroupButtons(
+             inputId = "person",
+             label = h3("Runner"),
+             choices = c("Bo", 
+                         "Rachael", "Adam"),
+             status = "success",
+             checkIcon = list(
+               yes = icon("ok", 
+                          lib = "glyphicon"),
+               no = icon("remove",
+                         lib = "glyphicon"))
+           )
+           )
+    
+  ),
+  fluidRow(
+    column(width = 6,
+           shinyWidgets::actionBttn(
+             inputId = "enter",
+             label = "Add Miles",
+             style = "unite", 
+             color = "danger"
+           )
+           )
+  ),
+  br(),
+  hr(),
+  
+  fluidRow(
+    column(width = 8,
+           plotOutput("plot")
+           )
+  ),
+  br(),
+  hr(),
+  fluidRow(
+    shiny::tableOutput("table")
+  )
+  
+  
+  
+)
