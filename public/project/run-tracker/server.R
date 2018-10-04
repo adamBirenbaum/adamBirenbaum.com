01e646410b54e428bb8e77b49a6bcb6c5ada7eb0
= <abirenbaum721@gmail.com>
2018-10-04 17:38:40 -0500

    removing legend label

--- public/project/run-tracker/server.R
+++ public/project/run-tracker/server.R
@@ -44,10 +44,13 @@ server <- function(input,output,session){
                                                                    Rachael= cumsum(Rachael),
                                                                    Adam = cumsum(Adam)) %>% 
       gather(key = "Person",value = "Miles",Bo,Rachael,Adam)
-  
-    ggplot(miles_df, aes(x = Date, y = Miles, group = Person, color = Person)) + geom_line(size = 2) +geom_point(size = 4)+ theme_minimal() + ggtitle("Running Progress")+
+ 
+    # browser()
+    # miles_df2 <- miles_df %>% group_by(Person) %>% mutate(txt = c(0,diff(Miles)))
+    # miles_df2$x <- 
+    ggplot(miles_df, aes(x = Date, y = Miles, group = Person, color = Person)) + geom_line(size = 2) +geom_point(size = 4) + theme_minimal() + ggtitle("Running Progress")+
       theme(axis.text = element_text(size = 14), axis.title = element_text(size = 18),plot.title = element_text(size = 28),legend.text = element_text(size = 18),
-            legend.position = "top", legend.title = element_text(size = 24))
+            legend.position = "top", legend.title = element_text(size = 24)) + scale_color_discrete("")
   })
   
   output$table <- renderTable({
@@ -85,7 +88,7 @@ server <- function(input,output,session){
       
       ggplot(miles_df, aes(x = Date, y = Miles, group = Person, color = Person)) + geom_line(size = 2) +geom_point(size = 4)+ theme_minimal() + ggtitle("Running Progress")+
         theme(axis.text = element_text(size = 14), axis.title = element_text(size = 18),plot.title = element_text(size = 28),legend.text = element_text(size = 18),
-              legend.position = "top", legend.title = element_text(size = 24))
+              legend.position = "top", legend.title = element_text(size = 24))+ scale_color_discrete("")
     })
     
     output$table <- renderTable({
