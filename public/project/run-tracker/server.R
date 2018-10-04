d4686cc83df1372a5b073fcc51e57373d603ced3
= <abirenbaum721@gmail.com>
2018-10-03 19:30:41 -0500

    changed legend, adde longest streak

--- public/project/run-tracker/server.R
+++ public/project/run-tracker/server.R
@@ -17,7 +17,24 @@ if (is_local){
 server <- function(input,output,session){
   phrases <- c("You Go Girl!","Great Job!","Way to Go!","Hell Ya!","You are amazing!","Woohooo","Yahooo!","Frick Ya!","Nice Work","Well Done!","Super Job!","Holy Cats!","Hod Dog, you are incredible!","Glory Hallelujah you are amazing!",
                "Out of Sight!","Good Golly!","JEEMINY CHRISTMAS!")
-  
+  get_streak <- function(x){
+    
+    streak <- 0
+    current_streak <- 0
+    for (i in x){
+      
+      if (i == 0){
+        if (current_streak > streak) streak <- current_streak
+        current_streak <- 0
+      }else{
+        current_streak <- current_streak + 1
+      }
+    }
+    
+    if (current_streak > streak) streak <- current_streak
+    
+    as.integer(streak)
+  }
   
   
   output$plot <- renderPlot({
@@ -28,8 +45,9 @@ server <- function(input,output,session){
                                                                    Adam = cumsum(Adam)) %>% 
       gather(key = "Person",value = "Miles",Bo,Rachael,Adam)
   
-    ggplot(miles_df, aes(x = Date, y = Miles, group = Person, color = Person)) + geom_line(size = 2) + theme_minimal() + ggtitle("Running Progress")+
-      theme(axis.text = element_text(size = 14), axis.title = element_text(size = 18),plot.title = element_text(size = 28),legend.text = element_text(size = 18))
+    ggplot(miles_df, aes(x = Date, y = Miles, group = Person, color = Person)) + geom_line(size = 2) +geom_point(size = 4)+ theme_minimal() + ggtitle("Running Progress")+
+      theme(axis.text = element_text(size = 14), axis.title = element_text(size = 18),plot.title = element_text(size = 28),legend.text = element_text(size = 18),
+            legend.position = "top", legend.title = element_text(size = 24))
   })
   
   output$table <- renderTable({
@@ -37,8 +55,12 @@ server <- function(input,output,session){
     miles_df <- miles_df %>% filter(Date <= today_date) 
     num_days <- nrow(miles_df) - 1
     table_df <- data.frame(Runner = c("Bo","Rachael","Adam"), "Total Miles" = c(sum(miles_df$Bo),sum(miles_df$Rachael), sum(miles_df$Adam)))
+
+
+    
     table_df <- table_df %>% mutate("Miles/Day" = Total.Miles/num_days)
     table_df[["Longest Run"]] <- c(max(miles_df$Bo),max(miles_df$Rachael), max(miles_df$Adam))
+    table_df[["Longest Streak"]] <- sapply(list(miles_df$Bo, miles_df$Rachael, miles_df$Adam), get_streak)
     table_df <- table_df[order(table_df$Total.Miles,decreasing = T),]
     names(table_df)[2] <- "Total Miles"
     table_df
@@ -61,8 +83,9 @@ server <- function(input,output,session){
                                                                      Adam = cumsum(Adam)) %>% 
         gather(key = "Person",value = "Miles",Bo,Rachael,Adam)
       
-      ggplot(miles_df, aes(x = Date, y = Miles, group = Person, color = Person)) + geom_line(size = 2) + theme_minimal() + ggtitle("Running Progress")+
-        theme(axis.text = element_text(size = 14), axis.title = element_text(size = 18),plot.title = element_text(size = 28),legend.text = element_text(size = 18))
+      ggplot(miles_df, aes(x = Date, y = Miles, group = Person, color = Person)) + geom_line(size = 2) +geom_point(size = 4)+ theme_minimal() + ggtitle("Running Progress")+
+        theme(axis.text = element_text(size = 14), axis.title = element_text(size = 18),plot.title = element_text(size = 28),legend.text = element_text(size = 18),
+              legend.position = "top", legend.title = element_text(size = 24))
     })
     
     output$table <- renderTable({
@@ -72,6 +95,7 @@ server <- function(input,output,session){
       table_df <- data.frame(Runner = c("Bo","Rachael","Adam"), "Total Miles" = c(sum(miles_df$Bo),sum(miles_df$Rachael), sum(miles_df$Adam)))
       table_df <- table_df %>% mutate("Miles/Day" = Total.Miles/num_days)
       table_df[["Longest Run"]] <- c(max(miles_df$Bo),max(miles_df$Rachael), max(miles_df$Adam))
+      table_df[["Longest Streak"]] <- sapply(list(miles_df$Bo, miles_df$Rachael, miles_df$Adam), get_streak)
       table_df <- table_df[order(table_df$Total.Miles,decreasing = T),]
       names(table_df)[2] <- "Total Miles"
       table_df
